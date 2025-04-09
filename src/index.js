5d5b81b9b2302cc88399ffba01b08bac60add8ad
JAYMSTUDIO16 <joonmin@Joonminui-MacBookPro.local>
2025-04-09 09:19:27 -0400

    Fix: reset package-lock.json and node_modules

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-<<<<<<< HEAD
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-=======
-root.render(<App />);
->>>>>>> ce9ef6e7de67e41e2104aaeea3cdada05602fff4
